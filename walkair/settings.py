1ea7a1ba6445f15c40b7e3002b070494d667bd26
Grant Audet <grant.audet@gmail.com>
2019-12-03 12:05:50 -0800

    static root

--- walkair/settings.py
+++ walkair/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'router'
 ]
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/static/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
