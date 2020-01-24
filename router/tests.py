139c2e6eb8f0f6c574949255c14cc501f2dc84e1
Grant Audet <grant.audet@gmail.com>
2020-01-23 18:37:17 -0800

    fixed selenium test

--- router/tests.py
+++ router/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.safari.webdriver import WebDriver 
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.webdriver import WebDriver 
+import time
+import pdb
 
 class SeleniumTests(StaticLiveServerTestCase):
 
@@ -9,6 +10,7 @@ class SeleniumTests(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
@@ -20,8 +22,20 @@ class SeleniumTests(StaticLiveServerTestCase):
         origin = self.selenium.find_elements_by_class_name('mapboxgl-ctrl-geocoder--input')[0]
         destination = self.selenium.find_elements_by_class_name('mapboxgl-ctrl-geocoder--input')[1]
         origin.send_keys("Ohlone Elementary School, 950 Amarillo Ave, Palo Alto, California 94303, United States of America")
+        self.selenium.find_element_by_css_selector('#origin ul li a').click()
         destination.send_keys("Ohlone Elementary School, 950 Amarillo Ave, Palo Alto, California 94303, United States of America")
-        self.selenium.find_element_by_tag_name('button').click()
+        self.selenium.find_element_by_css_selector('#destination ul li a').click()
+        self.assertFalse(self.selenium.find_element_by_css_selector('#routebutton button').is_enabled())
+        
+        destination.clear()
+        destination.send_keys("Duveneck Elementary, 705 Alester Ave, Palo Alto, California 94303, United States of America")
+        time.sleep(3)
+        self.selenium.find_element_by_css_selector('#destination ul li a').click()
 
+        time.sleep(3)
+        self.assertTrue(self.selenium.find_element_by_css_selector('#routebutton button').is_enabled())
 
-##Write a test case for gibberesh inputs and same origin and j
+### Testing out of bounds
+        self.selenium.find_element_by_css_selector('#routebutton button').click()
+        self.selenium.find_element_by_css_selector('.results button').click()
+        self.assertTrue('outside of the bounds' in self.selenium.find_element_by_css_selector('.results .card-body .card-subtitle').text)
